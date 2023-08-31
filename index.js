60db03265a9a47a1195cf41c378cce72a2f23bf8
rehan <Technosis141@gmail.com>
2023-08-30 23:31:26 -0400

    setting up react

--- index.js
+++ index.js
@@ -74,22 +74,20 @@ app.get("/callback", (req, res) => {
   })
     .then((response) => {
       if (response.status === 200) {
-        const { access_token, token_type } = response.data;
+        const { access_token, refresh_token } = response.data;
 
-        const { refresh_token } = response.data;
+        const queryParams = querystring.stringify({
+          access_token,
+          refresh_token,
+        });
 
-        axios
-          .get(
-            `http://localhost:8888/refresh_token?refresh_token=${refresh_token}`
-          )
-          .then((response) => {
-            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-          })
-          .catch((error) => {
-            res.send(error);
-          });
+        // redirect to react app
+        // pass along tokens in query params
+        res.redirect(`http://localhost:3000/?${queryParams} `)
+
+        
       } else {
-        res.send(response);
+        res.redirect(`/?${querystring.stringify({ error: "invalid_token" })}`);
       }
     })
     .catch((error) => {
